8e20e43feb901227f06974b0452e19e61e1138ae
luphumzo <mtukusel@gmail.com>
2018-08-07 17:06:34 +0200

    first commit

---
+++ java/main.js
@@ -0,0 +1,10 @@
+ Volume in drive C is Windows
+ Volume Serial Number is 1348-21C4
+
+ Directory of C:\Users\lifechoices\Documents\projects\CV\java
+
+2018/07/04  16:08    <DIR>          .
+2018/07/04  16:08    <DIR>          ..
+2018/07/04  16:08                 0 main.js
+               1 File(s)              0 bytes
+               2 Dir(s)  167ÿ109ÿ996ÿ544 bytes free
